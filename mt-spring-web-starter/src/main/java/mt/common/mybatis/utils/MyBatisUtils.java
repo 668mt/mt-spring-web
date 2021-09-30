9f05fef0b159c3433c027b38b260c45e1825a8f5
Martin <765936728@qq.com>
2021-09-30 10:50:50 +0800

    Revert "Message优化"
    This reverts commit 52bce5921f26ee7ec6973255eb5c99f7cf04d77c.

--- mt-spring-web-starter/src/main/java/mt/common/mybatis/utils/MyBatisUtils.java
+++ mt-spring-web-starter/src/main/java/mt/common/mybatis/utils/MyBatisUtils.java
@@ -1,6 +1,5 @@
 package mt.common.mybatis.utils;
 
-import mt.common.mybatis.entity.MtExample;
 import mt.common.mybatis.exception.NotSupportException;
 import mt.common.tkmapper.Filter;
 import mt.common.tkmapper.OrFilter;
@@ -93,16 +92,11 @@ public class MyBatisUtils {
 	 * @return
 	 */
 	public static Example createExample(Class<?> entityClass, List<Filter> filters) {
-		return createExample(entityClass, filters, false, null);
+		return createExample(entityClass, filters, false);
 	}
 	
 	public static Example createExample(Class<?> entityClass, List<Filter> filters, boolean forUpdate) {
-		return createExample(entityClass, filters, forUpdate, null);
-	}
-	
-	public static Example createExample(Class<?> entityClass, List<Filter> filters, boolean forUpdate, String resultMap) {
-		MtExample example = new MtExample(entityClass);
-		example.setResultMap(resultMap);
+		Example example = new Example(entityClass);
 		example.setForUpdate(forUpdate);
 		if (CollectionUtils.isNotEmpty(filters)) {
 			for (Filter filterInterface : filters) {
