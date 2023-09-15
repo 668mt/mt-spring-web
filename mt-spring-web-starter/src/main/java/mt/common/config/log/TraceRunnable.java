765da596fb28869aa57f62de9cca76d03829ac98
Administrator <765936728@qq.com>
2023-09-15 21:08:17 +0800

    升级1.0.3

---
+++ mt-spring-web-starter/src/main/java/mt/common/config/log/TraceRunnable.java
@@ -0,0 +1,36 @@
+package mt.common.config.log;
+
+import lombok.Getter;
+
+/**
+ * @Author Martin
+ * @Date 2023/9/15
+ */
+public class TraceRunnable implements Runnable {
+	@Getter
+	private final Runnable proxy;
+	private final String traceId;
+	
+	public TraceRunnable(Runnable proxy) {
+		this(proxy, TraceContext.getTraceId());
+	}
+	
+	public TraceRunnable(Runnable proxy, String traceId) {
+		if (proxy instanceof TraceRunnable) {
+			this.proxy = ((TraceRunnable) proxy).getProxy();
+		} else {
+			this.proxy = proxy;
+		}
+		this.traceId = traceId;
+	}
+	
+	@Override
+	public void run() {
+		try {
+			TraceContext.setTraceId(traceId);
+			proxy.run();
+		} finally {
+			TraceContext.removeTraceId();
+		}
+	}
+}
