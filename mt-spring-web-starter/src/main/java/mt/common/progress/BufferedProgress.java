69789ec592593a81818eef1c5a8bbaaa9d856855
Martin <765936728@qq.com>
2023-11-11 23:42:28 +0800

    升级1.0.3

--- mt-spring-web-starter/src/main/java/mt/common/progress/BufferedProgress.java
+++ mt-spring-web-starter/src/main/java/mt/common/progress/BufferedProgress.java
@@ -1,6 +1,7 @@
 package mt.common.progress;
 
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 import java.util.concurrent.atomic.AtomicInteger;
 
@@ -13,16 +14,29 @@ public class BufferedProgress implements Progress {
 	private final double bufferPercent;
 	private double lastPercent = 0;
 	private double currPercent = 0;
+	private ProgressListener progressListener;
 	
 	public BufferedProgress(@NotNull Progress delegate, double bufferPercent) {
+		this(delegate, bufferPercent, null);
+	}
+	
+	public BufferedProgress(@NotNull Progress delegate, double bufferPercent, @Nullable ProgressListener progressListener) {
 		this.delegate = delegate;
 		this.bufferPercent = bufferPercent;
+		this.progressListener = progressListener;
+	}
+	
+	private void listen(@NotNull String key) {
+		if (progressListener != null) {
+			progressListener.listen(key, getPercent(key));
+		}
 	}
 	
 	@Override
 	public void init(@NotNull String key) {
 		delegate.init(key);
 		lastPercent = 0d;
+		listen(key);
 	}
 	
 	@Override
@@ -30,6 +44,7 @@ public class BufferedProgress implements Progress {
 		if (percent - lastPercent >= bufferPercent || percent >= 0.9999) {
 			delegate.update(key, percent);
 			lastPercent = percent;
+			listen(key);
 		}
 		currPercent = percent;
 	}
@@ -40,6 +55,7 @@ public class BufferedProgress implements Progress {
 		if (currPercent - lastPercent >= bufferPercent || currPercent >= 0.9999) {
 			delegate.add(key, currPercent - lastPercent);
 			lastPercent = currPercent;
+			listen(key);
 		}
 	}
 	
@@ -63,14 +79,9 @@ public class BufferedProgress implements Progress {
 	public static void main(String[] args) {
 		LocalProgress localProgress = new LocalProgress();
 		AtomicInteger count = new AtomicInteger(0);
-		Progress progress = new ProgressListener(localProgress) {
-			@Override
-			public void listen(@NotNull String key, double percent) {
-				System.out.println(percent);
-				count.incrementAndGet();
-			}
-		};
-		BufferedProgress bufferedProgress = new BufferedProgress(progress, 0.5);
+		BufferedProgress bufferedProgress = new BufferedProgress(localProgress, 0.01, (key, percent) -> {
+			System.out.println(percent);
+		});
 		String key = "test";
 		PartProgress part1 = PartProgress.createPart(bufferedProgress, 0.5);
 		PartProgress part2 = PartProgress.createPart(bufferedProgress, 0.5);
@@ -81,6 +92,6 @@ public class BufferedProgress implements Progress {
 			part2.add(key, 0.0001);
 		}
 		System.out.println(count.get());
-		System.out.println("result:" + progress.getPercent(key));
+		System.out.println("result:" + bufferedProgress.getPercent(key));
 	}
 }
--- mt-spring-web-starter/src/main/java/mt/common/progress/ProgressListener.java
+++ mt-spring-web-starter/src/main/java/mt/common/progress/ProgressListener.java
@@ -2,44 +2,12 @@ package mt.common.progress;
 
 import org.jetbrains.annotations.NotNull;
 
-/**
- * @Author Martin
- * @Date 2023/11/11
- */
-public abstract class ProgressListener implements Progress {
-	private final Progress proxy;
-	
-	public ProgressListener(Progress proxy) {
-		this.proxy = proxy;
-	}
-	
+public interface ProgressListener {
 	/**
 	 * 监听进度
 	 *
-	 * @param key     进度key
-	 * @param percent 进度百分比
+	 * @param key
+	 * @param percent
 	 */
-	public abstract void listen(@NotNull String key, double percent);
-	
-	@Override
-	public void update(@NotNull String key, double percent) {
-		proxy.update(key, percent);
-		listen(key, getPercent(key));
-	}
-	
-	@Override
-	public void add(@NotNull String key, double percent) {
-		proxy.add(key, percent);
-		listen(key, getPercent(key));
-	}
-	
-	@Override
-	public void remove(@NotNull String key) {
-		proxy.remove(key);
-	}
-	
-	@Override
-	public double getPercent(@NotNull String key) {
-		return proxy.getPercent(key);
-	}
+	void listen(@NotNull String key, double percent);
 }
