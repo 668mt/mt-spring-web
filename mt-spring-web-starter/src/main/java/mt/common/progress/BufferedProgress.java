984bb87e21fb2552f986591b9556ca10b42c122f
Martin <765936728@qq.com>
2023-11-11 23:27:04 +0800

    升级1.0.3

--- mt-spring-web-starter/src/main/java/mt/common/progress/BufferedProgress.java
+++ mt-spring-web-starter/src/main/java/mt/common/progress/BufferedProgress.java
@@ -1,60 +1,86 @@
-//package mt.common.progress;
-//
-//import org.jetbrains.annotations.NotNull;
-//
-///**
-// * @Author Martin
-// * @Date 2023/4/5
-// */
-//public class BufferedProgress implements Progress {
-//	private final Progress delegate;
-//	private final double bufferPercent;
-//	private double lastPercent = 0;
-//	private double currPercent = 0;
-//
-//	public BufferedProgress(@NotNull Progress delegate, double bufferPercent) {
-//		this.delegate = delegate;
-//		this.bufferPercent = bufferPercent;
-//	}
-//
-//	@Override
-//	public void init(@NotNull String key) {
-//		delegate.init(key);
-//		lastPercent = 0d;
-//	}
-//
-//	@Override
-//	public void update(@NotNull String key, double percent) {
-//		if (percent - lastPercent >= bufferPercent) {
-//			delegate.update(key, percent);
-//			lastPercent = percent;
-//		}
-//		currPercent = percent;
-//	}
-//
-//	@Override
-//	public void add(@NotNull String key, double addPercent) {
-//		currPercent += addPercent;
-//		if (currPercent - lastPercent >= bufferPercent) {
-//			delegate.add(key, currPercent - lastPercent);
-//			lastPercent = currPercent;
-//		}
-//	}
-//
-//	@Override
-//	public void remove(@NotNull String key) {
-//		delegate.remove(key);
-//		lastPercent = 0;
-//		currPercent = 0;
-//	}
-//
-//	@Override
-//	public double getPercent(@NotNull String key) {
-//		return delegate.getPercent(key);
-//	}
-//
-//	@Override
-//	public void finish(@NotNull String key) {
-//		delegate.finish(key);
-//	}
-//}
+package mt.common.progress;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+/**
+ * @Author Martin
+ * @Date 2023/4/5
+ */
+public class BufferedProgress implements Progress {
+	private final Progress delegate;
+	private final double bufferPercent;
+	private double lastPercent = 0;
+	private double currPercent = 0;
+	
+	public BufferedProgress(@NotNull Progress delegate, double bufferPercent) {
+		this.delegate = delegate;
+		this.bufferPercent = bufferPercent;
+	}
+	
+	@Override
+	public void init(@NotNull String key) {
+		delegate.init(key);
+		lastPercent = 0d;
+	}
+	
+	@Override
+	public void update(@NotNull String key, double percent) {
+		if (percent - lastPercent >= bufferPercent || percent >= 0.9999) {
+			delegate.update(key, percent);
+			lastPercent = percent;
+		}
+		currPercent = percent;
+	}
+	
+	@Override
+	public void add(@NotNull String key, double addPercent) {
+		currPercent += addPercent;
+		if (currPercent - lastPercent >= bufferPercent || currPercent >= 0.9999) {
+			delegate.add(key, currPercent - lastPercent);
+			lastPercent = currPercent;
+		}
+	}
+	
+	@Override
+	public void remove(@NotNull String key) {
+		delegate.remove(key);
+		lastPercent = 0;
+		currPercent = 0;
+	}
+	
+	@Override
+	public double getPercent(@NotNull String key) {
+		return delegate.getPercent(key);
+	}
+	
+	@Override
+	public void finish(@NotNull String key) {
+		delegate.finish(key);
+	}
+	
+	public static void main(String[] args) {
+		LocalProgress localProgress = new LocalProgress();
+		AtomicInteger count = new AtomicInteger(0);
+		Progress progress = new ProgressListener(localProgress) {
+			@Override
+			public void listen(@NotNull String key, double percent) {
+				System.out.println(percent);
+				count.incrementAndGet();
+			}
+		};
+		BufferedProgress bufferedProgress = new BufferedProgress(progress, 0.5);
+		String key = "test";
+		PartProgress part1 = PartProgress.createPart(bufferedProgress, 0.5);
+		PartProgress part2 = PartProgress.createPart(bufferedProgress, 0.5);
+		for (int i = 0; i < 10000; i++) {
+			part1.add(key, 0.0001);
+		}
+		for (int i = 0; i < 10000; i++) {
+			part2.add(key, 0.0001);
+		}
+		System.out.println(count.get());
+		System.out.println("result:" + progress.getPercent(key));
+	}
+}
